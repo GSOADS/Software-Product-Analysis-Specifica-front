dc3370b230f7866dddef0607ec187b8cb0ddbd33
GSOADS <gabrieldesantanasp2010@hotmail.com>
2024-10-05 19:26:14 -0300

    criação do botão editar e salvar e trava para não buscar mais itens de lista

--- paginas/alterarLista/alterarLista.js
+++ paginas/alterarLista/alterarLista.js
@@ -5,6 +5,7 @@ const urlserverGetObterLista="http://localhost:8080/lista/obterListaEItens/"
 const UrlserverPost2= "http://localhost:8080/produtos/lista2"
 var dbNomeDaLista=dbNomeDaLista;
 var dbLista=dbLista;
+var buscar_realizada_sucesso=false;
 
 
 var conta=0;
@@ -19,6 +20,8 @@ document.addEventListener("keydown", function(event)
 
 function buscar()
 { 
+  if(buscar_realizada_sucesso==false)
+  {
   var input_lista_nomelista = document.getElementById(`inputnomelista`).value
   console.log(input_lista_nomelista);
   console.log(input_lista_nomelista+toString(urlserverGetObterLista));
@@ -46,12 +49,19 @@ function buscar()
 
         //alert(console.log(dbLista,dbNomeDaLista));//habilitar para teste
         constroiTabela(dbNomeDaLista,dbLista);
+        buscar_realizada_sucesso=true;
+        alert(buscar_realizada_sucesso,console.log("busca realizada com sucesso",buscar_realizada_sucesso));
       
       })
+    
 
     }
+  }
 
-    function constroiTabela(dbNomeDaLista, dbLista) {
+    function constroiTabela(dbNomeDaLista, dbLista) 
+    {
+      
+      
       console.log("dbNomeDaLista", dbNomeDaLista.nomeDaSuaLista);
       console.log("dbNomeDaLista", dbNomeDaLista.id);
   
@@ -82,6 +92,7 @@ function buscar()
     }
 
 
+
       function constroilista()
       {
         
@@ -107,7 +118,7 @@ function buscar()
         input_lista_id.placeholder ="id";
         input_lista_id.value=dbLista[i].id;
         input_lista_id.readOnly=true;
-        input_lista_id.labels="id";
+        input_lista_id.className= "formater"
 
         var input_lista_idDbNomeLista = document.createElement("input");
         input_lista_idDbNomeLista.type="text";
@@ -116,6 +127,7 @@ function buscar()
         input_lista_idDbNomeLista.placeholder ="id";
         input_lista_idDbNomeLista.value=dbLista[i].idDbNomeLista;
         input_lista_idDbNomeLista.readOnly=true;
+        input_lista_idDbNomeLista.className= "formater";
 
         var 
         input_lista_nomeDaSuaLista = document.createElement("input");
@@ -153,6 +165,13 @@ function buscar()
         input_lista_sugestaoDeLugar.value=dbLista[i].sugestaoDeLugar;
         input_lista_sugestaoDeLugar.readOnly=true;
 
+        // var botao_editar_lista = document.createElement("button");
+        // botao_editar_lista.innerHTML = "Editar";
+        // botao_editar_lista.onclick = aoClicar;
+        // // botao_editar_lista.className="botaosEditar";
+        // botao_editar_lista.id="botaosEditar";
+        // divlista2.appendChild(botao_editar_lista);
+
 
         console.log(input_lista_id,input_lista_idDbNomeLista,input_lista_itemLista,
           input_lista_nomeDaSuaLista,input_lista_quemVaiComprar,input_lista_sugestaoDeLugar)
@@ -164,10 +183,42 @@ function buscar()
         divlista2.appendChild(input_lista_quemVaiComprar);
         divlista2.appendChild(input_lista_sugestaoDeLugar);
 
+        
+
+          
+          
+
         document.getElementById("conteudo_itemlista_linha").appendChild(divlista2);
+
+       // Adiciona o botão de editar
+    
          
 
       }
+
+      let divlistabutton = document.createElement("div");
+      divlistabutton.id = "divListaButton";
+
+      
+
+      let botao_editar_lista = document.createElement("button");
+          botao_editar_lista.innerHTML = "Editar";
+          botao_editar_lista.onclick = editar;
+          // botao_editar_lista.className="botaosEditar";
+          botao_editar_lista.id="botaosEditar";
+
+          let botao_salvar_lista = document.createElement("button");
+          botao_salvar_lista.innerHTML = "Salvar";
+          botao_salvar_lista.onclick = salvar;
+          // botao_editar_lista.className="botaosEditar";
+          botao_salvar_lista.id="botaosEditar";  
+
+
+
+          
+          document.getElementById("conteudo_itemlista_linha").appendChild(divlistabutton);
+          document.getElementById("divListaButton").appendChild(botao_editar_lista);
+          document.getElementById("divListaButton").appendChild(botao_salvar_lista);
 
       
 
@@ -188,263 +239,11 @@ function buscar()
 
 
   }
+  function editar() {
     
-
-
-
-
-
-        
-
-//   "dbNomeDaLista": {
-//     "id": 2,
-//     "nomeDaSuaLista": "teste 3"
-// },
-// "dbLista": [
-//     {
-//         "id": 2,
-//         "idDbNomeLista": 2,
-//         "nomeDaSuaLista": "teste 3",
-//         "itemLista": "teste 1",
-//         "quemVaiComprar": "novo teste",
-//         "sugestaoDeLugar": "santo"
-//     },
-//     {
-
-  // console.log(container)
-//   var divlista=document.createElement("div");
-//   divlista.id = "divlista";
-//   // divlista.textContent ="NOVA DIV";
-//   console.log(divlista)
-//   console.log(conta);
-//   // console.log(input_lista_numero.id)
-  
-//   // input_lista_item.id,input_lista_compra.id,input_lista_local.id);
-
-//   var input_lista_numero = document.createElement("input");
-//   input_lista_numero.type="text";
-//   input_lista_numero.id=`input_numero${conta}`;
-//   input_lista_numero.class="inputlistamodelo";
-//   input_lista_numero.placeholder ="Posição";
-//   input_lista_numero.value=`${conta}`;
-//   input_lista_numero.readOnly=true;
-
-
-//   var input_lista_item = document.createElement("input");
-//   input_lista_item.type="text";
-
-//   input_lista_item.id=`input_item${conta}`;
-//   input_lista_item.class="inputlistamodelo";
-//   input_lista_item.placeholder ="Item lista"
-  
-
-//   var input_lista_compra = document.createElement("input");
-//   input_lista_compra.type="text";
-//   input_lista_compra.id=`input_compra${conta}`;
-//   input_lista_compra.class="inputlistamodelo";
-//   input_lista_compra.placeholder ="Quem vai comprar?"
-
-//   var input_lista_local = document.createElement("input");
-//   input_lista_local.type="text";
-//   input_lista_local.id=`input_local${conta}`;
-//   input_lista_local.class="inputlistamodelo";
-//   input_lista_local.placeholder ="Sugestão de lugar"
-
-//   divlista.appendChild(input_lista_numero);
-//   divlista.appendChild(input_lista_item);
-//   divlista.appendChild(input_lista_compra);
-//   divlista.appendChild(input_lista_local);
-  
-
-
-
-//   // input_lista_compra,input_lista_item,input_lista_local,
-
-//   document.getElementById("conteudo_itemlista_linha").appendChild(divlista);
-  
-  
-//   conta++;
-
-// }
-
-// function botaosalvar()
-// {
-//   var dbLista = new Array();
-//   var dbNomeDaLista;
-  
-
-//   for (var quantidade_linhas =0; quantidade_linhas< conta;quantidade_linhas++)
-//   {
-//     // Obtém os valores dos inputs
-//     var input_lista_numero = document.getElementById(`input_numero${quantidade_linhas}`).value;
-//     var input_lista_compra = document.getElementById(`input_compra${quantidade_linhas}`).value;
-//     var input_lista_local = document.getElementById(`input_local${quantidade_linhas}`).value;
-//     var input_lista_item = document.getElementById(`input_item${quantidade_linhas}`).value;
-//     var input_lista_nomelista = document.getElementById(`inputnomelista`).value;
-
-
-//      // Cria um objeto JSON com os valores obtidos
-
-//     var lista = 
-//     {
-//       // "numero":input_lista_numero,
-//       "quemVaiComprar":input_lista_compra,
-//       "sugestaoDeLugar":input_lista_local,
-//       "itemLista":input_lista_item,
-//       // "nomeLista":input_lista_nomelista,
-//     };
-
-//     dbNomeDaLista=
-//     {
-//       "nomeLista":input_lista_nomelista
-
-//     };
-
-
-//     //Salva o valor das informaçoes do json no array
-
-//     dbLista[quantidade_linhas] = lista;
+    alert("O botão editar foi clicado!");
+  }
+  function salvar() {
     
-
-
-
-//     // console.log(quantidade_linhas);
-//   }
-
-//   const data = {
-//     "dbNomeDaLista": {
-//       "nomeDaSuaLista": dbNomeDaLista.nomeLista
-//     },
-//     "dbLista": dbLista
-//   };
-  
-  
- 
-
-  
- 
-  
-
-   
-
-
-//   // console.log({dbNomeDaLista,dbLista});
-//   // console.log(dbNomeDaLista.constructor.name);
-//   // console.log(dbLista.constructor.name);
-
-
-//   console.log("Estou no POST")
-//   const configuracao = {
-//   method: 'POST',
-//   headers: {
-//       'Content-Type': 'application/json',
-//       'headerLista': 'Dado'
-//   },
-//   body: JSON.stringify(data
-    
-      
-//       // "dbLista":[dbLista]
-
-    
-
-
-// //     {"dbNomeDaLista": {
-// //     "nomeDaSuaLista":"Lista de presente do Gabriel"},
-// //     "dbLista": 
-// //   [
-// //     {
-// //  "itemLista":"PLAYSTATION5510",
-// //  "quemVaiComprar":"TAISE",
-// //  "sugestaoDeLugar":"MAGAZINE" },
-// // {
-  
-// //  "itemLista":"PLAYSTATION10",
-// //  "quemVaiComprar":"GEPETO",
-// //  "sugestaoDeLugar":"MAGAZINE"
-
-// // }
-// //   ]}
-
-// )
-// };
-
-// // Enviando a requisição
-// fetch(UrlserverPostCriarLista, configuracao)
-//   // .then(response => response.json())
-//   .then(data => {
-//     console.log('Sucesso:', data);
-//     alert('Sucesso: A operação foi concluída com êxito!');
-//     limparLista();
-
-    
-//   })
-//   .catch(error => console.error('Erro:', error));
-
-// }
-
-// function limparLista()
-// {
-//   window.location.href = '/paginas/criarLista/criarLista.html';
-
-
-
-// }
-
-
-
-
-// let divusuario = document.getElementById("aqui_json")
-
-// // function POST()
-// // {
-// // fetch(Urlserver, 
-// //     {
-// //     method: 'POST',
-// //     headers: {
-
-// //       'Content-Type': 'application/json'
-// //       // Puedes ajustar los encabezados según tus necesidades
-// //     },
-// //     body: JSON.stringify({nome:"ola"})
-// //   })
-// //   .then(response => response.json())
-// //   .then(data => {
-// //     // Almacenar la respuesta en una variable
-// //     var respuestaJSON = data;
-// //     // Mostrar la respuesta en una div
-// //     var resultadoDiv = document.getElementById('aqui_json');
-// //     resultadoDiv.inputMode = JSON.stringify(respuestaJSON);
-// //   })
-// //   .catch(error => {
-// //     console.error('Error:', error);
-// //   });
-// // }
-
-// // function POST()
-// // {
-// // fetch(Urlserver,{
-// //     headers:
-// //     {
-// //         'Accept':'application/json',
-// //         'Content-Type': 'application/json'
-// //     },
-// //     method:"Post",
-// //         body: JSON.stringify (
-// //             {nome:"ola"
-               
-// //             })
-
-// //     //converte string para json
-// // }).then((response)=>
-// // {response.json().
-// //     then((dados)=>
-// //     {console.log(dados)
-// //         //let recebidos = dados//divusuario.innerHTML+=`<li>${response.JSON}</li>`
-
-// //         //divusuario.innerHTML += usurios
-// //         console.log(recebidos)
-// //         //console.log
-    
-// //     })
-// // }
-// // )}
+    alert("O botão salvar foi clicado!");
+  }
